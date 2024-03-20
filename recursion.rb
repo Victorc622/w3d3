c292fa60ac3035a24c0e83a7cc9281af9fb69b3c
katie leong <katieleong@yahoo.com>
2024-03-20 12:12:17 -0700

    exponentiation

--- recursion.rb
+++ recursion.rb
@@ -27,4 +27,28 @@ def range(num_1, num_2)
     return result
    end
 end
-p range(1, 5)
+# p range(1, 5)
+
+# def return_number(a)
+#     ((a / 2)**2) * (65 - (a / 2))
+# end
+
+def return_number(a)
+    childs_age = a / 2
+    years_until_retirement = 65 - childs_age
+    money_in_bank = childs_age ** 2
+    money_at_retirement = money_in_bank * years_until_retirement
+    return money_at_retirement
+end
+
+p return_number(30)
+
+def exponentiation(base, number)
+    return 1 if number == 0
+
+    
+
+end
+
+exp(0,0) # (base, power) = 1
+exp(0,1) #
